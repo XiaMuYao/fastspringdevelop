871101f11eca309641b15d71ae43287530a4e40a
夏沐尧 <xiamuyaodqx@gmail.com>
2018-12-20 00:38:33 +0800

    定时任务

--- src/main/java/top/xiamuyao/fastspringdevelop/FastspringdevelopApplication.java
+++ src/main/java/top/xiamuyao/fastspringdevelop/FastspringdevelopApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,11 +12,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @ServletComponentScan
+@EnableScheduling
 @MapperScan("top.xiamuyao.fastspringdevelop.api.mapper")
 public class FastspringdevelopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FastspringdevelopApplication.class, args);
-		System.out.println("接口文档地址: http://localhost:8383/swagger-ui.html");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FastspringdevelopApplication.class, args);
+        System.out.println("接口文档地址: http://localhost:8383/swagger-ui.html");
+    }
 }
